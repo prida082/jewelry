2c44d10b1f7ea5449d8ab0585c7045b21da48911
prida082 <beatbrave882@gmail.com>
2022-01-24 00:46:36 +0700

    new10

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//api Authen
+Route::post('/login',[\App\Http\Controllers\AuthController::class,'login']);
+Route::post('/logout',[\App\Http\Controllers\AuthController::class,'logout']);
+Route::post('/register',[\App\Http\Controllers\AuthController::class,'register']);
+
+//public
+Route::get('/product',[\App\Http\Controllers\ProductController::class,'index']);
+Route::get('/product/show/{id}',[\App\Http\Controllers\ProductController::class,'show']);
+Route::get('/product/search/{name}',[\App\Http\Controllers\ProductController::class,'search']);
+
+//product
+
+// Route::group(['middleware'=>['auth:sanctum']],function(){
+//     Route::post('/product/store',[\App\Http\Controllers\ProductController::class,'store']);
+//     Route::post('/product/update',[\App\Http\Controllers\ProductController::class,'update']);
+// });
+Route::middleware('auth:sanctum')->post('/product/store',[\App\Http\Controllers\ProductController::class,'store']);
+Route::middleware('auth:sanctum')->post('/product/update',[\App\Http\Controllers\ProductController::class,'update']);
+Route::delete('/product/{id}',[\App\Http\Controllers\ProductController::class,'delete']);
